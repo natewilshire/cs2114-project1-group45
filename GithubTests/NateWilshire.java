575fd25bc94958df8480a35a8c7b1156da7af6e0
shafiursarkerVTCS <shafiursarker@vt.edu>
2026-09-01 19:06:27 -0400

    Basic setup
    setup the environment and tested the push

--- GithubTests/NateWilshire.java
+++ GithubTests/NateWilshire.java
@@ -1,3 +1,4 @@
+package GithubTests;
 public class NateWilshire {
     public static void main(String[] args) {
         System.out.println("Nate is on the team.");
---
+++ GithubTests/ShafiurSarker.java
@@ -0,0 +1,16 @@
+package GithubTests;
+
+// -------------------------------------------------------------------------
+/**
+ * basic github push test
+ * 
+ * @author shafiursarker
+ * @version Sep 1, 2026
+ */
+public class ShafiurSarker
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Your Name is on the team.");
+    }
+}
